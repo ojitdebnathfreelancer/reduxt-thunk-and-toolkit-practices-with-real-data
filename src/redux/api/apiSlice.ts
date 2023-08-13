c67378c217b124996871895c3d9d75b9a642f8cf
unknown <ojitdebnathfreelancer.com>
2023-08-13 21:52:49 +0600

    login and signup page modified and comment post api created

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -12,7 +12,18 @@ export const api = createApi({
     singleProducts: builder.query({
       query: (id) => `product/${id}`,
     }),
+    postComment: builder.mutation({
+      query: ({ id, ...data }) => ({
+        url: `comment/${id}`,
+        method: 'POST',
+        body: data,
+      }),
+    }),
   }),
 });
 
-export const { useGetProductsQuery, useSingleProductsQuery } = api;
+export const {
+  useGetProductsQuery,
+  useSingleProductsQuery,
+  usePostCommentMutation,
+} = api;
