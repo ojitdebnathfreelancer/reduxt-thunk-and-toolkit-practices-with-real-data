16337b36ce8bd4bbfef8429147f09c61db526cd3
unknown <ojitdebnathfreelancer.com>
2023-08-15 12:17:05 +0600

    signup and login done with firebase

--- src/redux/features/userSlice/userSlice.ts
+++ src/redux/features/userSlice/userSlice.ts
@@ -1,4 +1,10 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { auth } from '@/lib/firebase';
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
 
 interface IUserState {
   user: {
@@ -7,6 +13,11 @@ interface IUserState {
   isLoading: boolean;
   isError: boolean;
   error: string | null;
+}
+
+interface iCredential {
+  email: string;
+  password: string;
 }
 
 const initialState: IUserState = {
@@ -18,10 +29,64 @@ const initialState: IUserState = {
   error: null,
 };
 
+export const createUser = createAsyncThunk(
+  'user/createUser',
+  async ({ email, password }: iCredential) => {
+    const data = await createUserWithEmailAndPassword(auth, email, password);
+    return data.user.email;
+  }
+);
+
+export const loginUser = createAsyncThunk(
+  'user/loginUser',
+  async ({ email, password }: iCredential) => {
+    const data = await signInWithEmailAndPassword(auth, email, password);
+    return data.user.email;
+  }
+);
+
 const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(createUser.pending, (state) => {
+        state.isLoading = true;
+        state.isError = false;
+        state.error = null;
+      })
+      .addCase(createUser.fulfilled, (state, action) => {
+        state.user.email = action.payload;
+        state.isLoading = false;
+        state.isError = false;
+        state.error = null;
+      })
+      .addCase(createUser.rejected, (state, action) => {
+        state.user.email = null;
+        state.isLoading = false;
+        state.isError = true;
+        state.error = action.payload.message;
+      })
+
+      .addCase(loginUser.pending, (state) => {
+        state.isLoading = true;
+        state.isError = false;
+        state.error = null;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.user.email = action.payload;
+        state.isLoading = false;
+        state.isError = false;
+        state.error = null;
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.user.email = null;
+        state.isLoading = false;
+        state.isError = true;
+        state.error = action.payload.message;
+      });
+  },
 });
 
 export default userSlice.reducer;
